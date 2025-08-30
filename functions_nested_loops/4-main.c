3a8f6f8afffd4ab44fa2b066514c9008b46ab5f5
layan <layansaleem@gmail.com>
2025-08-30 14:47:18 +0000

    4

---
+++ functions_nested_loops/4-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = _isalpha('H');
+    printf("%d\n", r);
+    r = _isalpha('o');
+    printf("%d\n", r);
+    r = _isalpha(108);
+    printf("%d\n", r);
+    r = _isalpha(';');
+    printf("%d\n", r);
+    return (0);
+}
