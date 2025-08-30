5f39ac88ed27a3866cc793bfb164656f9abd66e5
layan <layansaleem@gmail.com>
2025-08-30 15:42:52 +0000

    7

---
+++ functions_nested_loops/7-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = print_last_digit(98);
+    printf("%d\n", r);
+    r = print_last_digit(0);
+    printf("%d\n", r);
+    r = print_last_digit(-1024);
+    printf("%d\n", r);
+    return (0);
+}
