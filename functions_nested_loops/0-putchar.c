efac16e28f9d6dbb6dc8b08e34b794de1dfd6771
layan <layansaleem@gmail.com>
2025-08-30 13:15:37 +0000

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - Prints _putchar followed by a new line
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	printf("_putchar\n");
-	return (0);
+    printf("_putchar\n");
+    return (0);
 }
