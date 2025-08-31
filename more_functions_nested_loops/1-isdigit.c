bfe47fee5eac48309c48168a8add7f4cb5b56eee
layan <layansaleem@gmail.com>
2025-08-31 08:51:44 +0000

    1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 
-/**
- * _isdigit - Checks for a digit (0 through 9)
- * @c: The character to be checked
- *
- * Return: 1 if c is a digit, 0 otherwise
- */
-int _isdigit(int c)
+int main(void)
 {
-	return (c >= '0' && c <= '9');
+    printf("Testing _isdigit function:\n");
+    
+    // Test digits (should return 1)
+    for (char c = '0'; c <= '9'; c++)
+    {
+        printf("'%c': %d\n", c, _isdigit(c));
+    }
+    
+    // Test non-digits (should return 0)
+    printf("'a': %d\n", _isdigit('a'));
+    printf("'A': %d\n", _isdigit('A'));
+    printf("'@': %d\n", _isdigit('@'));
+    printf("' ': %d\n", _isdigit(' '));
+    
+    return (0);
 }
