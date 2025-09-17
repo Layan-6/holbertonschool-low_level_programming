9dab47d58c507426d4a715d839feb641504cb9be
layan <layansaleem@gmail.com>
2025-09-17 14:25:20 +0000

    1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -11,42 +11,34 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *result;
-    unsigned int len1 = 0, len2 = 0, i, j;
+	char *result;
+	unsigned int len1 = 0, len2 = 0, i, j;
 
-    /* Treat NULL as empty string */
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    /* Calculate length of s1 manually */
-    while (s1[len1] != '\0')
-        len1++;
+	while (s1[len1] != '\0')
+		len1++;
 
-    /* Calculate length of s2 manually */
-    while (s2[len2] != '\0')
-        len2++;
+	while (s2[len2] != '\0')
+		len2++;
 
-    /* If n is greater than s2 length, use entire s2 */
-    if (n >= len2)
-        n = len2;
+	if (n >= len2)
+		n = len2;
 
-    /* Allocate memory for result */
-    result = malloc(len1 + n + 1);
-    if (result == NULL)
-        return (NULL);
+	result = malloc(len1 + n + 1);
+	if (result == NULL)
+		return (NULL);
 
-    /* Copy s1 to result */
-    for (i = 0; i < len1; i++)
-        result[i] = s1[i];
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
 
-    /* Copy first n bytes of s2 to result */
-    for (j = 0; j < n; j++)
-        result[i + j] = s2[j];
+	for (j = 0; j < n; j++)
+		result[i + j] = s2[j];
 
-    /* Null terminate the string */
-    result[i + j] = '\0';
+	result[i + j] = '\0';
 
-    return (result);
+	return (result);
 }
