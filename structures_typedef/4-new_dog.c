4e6ab66135aa76d595a66fbcf5b6659ef7684d26
layan <layansaleem@egmail.com>
2025-09-18 07:33:27 +0000

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,6 +2,41 @@
 #include "dog.h"
 
 /**
+ * _strlen - Calculates the length of a string
+ * @str: The string to measure
+ *
+ * Return: Length of the string
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (str[len])
+		len++;
+	return (len);
+}
+
+/**
+ * _strcopy - Copies a string
+ * @dest: Destination buffer
+ * @src: Source string
+ *
+ * Return: Pointer to destination
+ */
+char *_strcopy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
  * new_dog - Creates a new dog
  * @name: Name of the dog
  * @age: Age of the dog
@@ -13,33 +48,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 	char *name_copy, *owner_copy;
-	int name_len = 0, owner_len = 0, i;
+	int name_len, owner_len;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
 
-	/* Calculate lengths of name and owner */
-	while (name[name_len])
-		name_len++;
-	while (owner[owner_len])
-		owner_len++;
-
-	/* Allocate memory for new dog */
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 
-	/* Allocate memory and copy name */
+	name_len = _strlen(name);
+	owner_len = _strlen(owner);
+
 	name_copy = malloc(sizeof(char) * (name_len + 1));
 	if (name_copy == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-	for (i = 0; i <= name_len; i++)
-		name_copy[i] = name[i];
+	_strcopy(name_copy, name);
 
-	/* Allocate memory and copy owner */
 	owner_copy = malloc(sizeof(char) * (owner_len + 1));
 	if (owner_copy == NULL)
 	{
@@ -47,10 +75,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog);
 		return (NULL);
 	}
-	for (i = 0; i <= owner_len; i++)
-		owner_copy[i] = owner[i];
+	_strcopy(owner_copy, owner);
 
-	/* Initialize the new dog */
 	new_dog->name = name_copy;
 	new_dog->age = age;
 	new_dog->owner = owner_copy;
