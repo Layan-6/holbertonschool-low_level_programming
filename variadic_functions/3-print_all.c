33e60a1482044c9ecf5a187fdc49fb0d54e93706
layan <layansaleem@gmail.com>
2025-09-27 11:55:34 +0000

    3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,41 +3,6 @@
 #include "variadic_functions.h"
 
 /**
- * print_arg - Prints a single argument based on type
- * @type: The type of argument (c, i, f, s)
- * @args: The va_list of arguments
- * @separator: Pointer to separator flag
- */
-void print_arg(char type, va_list args, int *separator)
-{
-	char *str;
-
-	if (*separator)
-		printf(", ");
-	*separator = 1;
-
-	switch (type)
-	{
-		case 'c':
-			printf("%c", va_arg(args, int));
-			break;
-		case 'i':
-			printf("%d", va_arg(args, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(args, double));
-			break;
-		case 's':
-			str = va_arg(args, char *);
-			if (str == NULL)
-				printf("(nil)");
-			else
-				printf("%s", str);
-			break;
-	}
-}
-
-/**
  * print_all - Prints anything based on the format
  * @format: List of types of arguments
  *
@@ -48,24 +13,45 @@ void print_all(const char * const format, ...)
 {
 	va_list args;
 	unsigned int i = 0;
+	char *str;
 	char current;
 	int separator = 0;
 
 	va_start(args, format);
 
-	if (format == NULL)
-	{
-		printf("\n");
-		va_end(args);
-		return;
-	}
-
-	while (format[i])
+	while (format && format[i])
 	{
 		current = format[i];
 
 		if (current == 'c' || current == 'i' || current == 'f' || current == 's')
-			print_arg(current, args, &separator);
+		{
+			if (separator)
+				printf(", ");
+
+			switch (current)
+			{
+				case 'c':
+					printf("%c", va_arg(args, int));
+					separator = 1;
+					break;
+				case 'i':
+					printf("%d", va_arg(args, int));
+					separator = 1;
+					break;
+				case 'f':
+					printf("%f", va_arg(args, double));
+					separator = 1;
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					if (!str)
+						printf("(nil)");
+					else
+						printf("%s", str);
+					separator = 1;
+					break;
+			}
+		}
 		i++;
 	}
 
