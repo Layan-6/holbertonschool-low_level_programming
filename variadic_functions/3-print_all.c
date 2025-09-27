eccf6e4ae1167591332e59df408f270d496590a2
layan <layansaleem@gmail.com>
2025-09-27 16:23:45 +0000

    3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -16,11 +16,12 @@ void print_all(const char * const format, ...)
 	va_start(args, format);
 	while (format && format[i])
 	{
-		if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+		if (format[i] == 'c' || format[i] == 'i' || 
+		    format[i] == 'f' || format[i] == 's')
 		{
 			if (sep)
 				printf(", ");
-			
+
 			switch (format[i])
 			{
 				case 'c':
