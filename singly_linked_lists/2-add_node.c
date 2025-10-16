90556668a607b24c308f6ae1e44538b756a3e5fe
layan <layan@gmail.com>
2025-10-16 14:46:12 +0000

    2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -3,6 +3,54 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - calculates the length of a string
+ * @str: string to calculate length of
+ *
+ * Return: length of the string
+ */
+unsigned int _strlen(const char *str)
+{
+	unsigned int len = 0;
+
+	if (str == NULL)
+		return (0);
+
+	while (str[len])
+		len++;
+
+	return (len);
+}
+
+/**
+ * _strdup - duplicates a string
+ * @str: string to duplicate
+ *
+ * Return: pointer to duplicated string, or NULL if failed
+ */
+char *_strdup(const char *str)
+{
+	char *dup;
+	unsigned int i, len;
+
+	if (str == NULL)
+	{
+		dup = strdup("(nil)");
+		return (dup);
+	}
+
+	len = _strlen(str);
+	dup = malloc(sizeof(char) * (len + 1));
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		dup[i] = str[i];
+	dup[i] = '\0';
+
+	return (dup);
+}
+
+/**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: double pointer to the head of the list
  * @str: string to be duplicated and added to the new node
@@ -13,33 +61,14 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 	char *str_dup;
-	unsigned int len = 0;
 
 	if (head == NULL)
 		return (NULL);
 
-	/* Calculate string length */
-	if (str != NULL)
-	{
-		while (str[len])
-			len++;
-	}
-
-	/* Duplicate the string */
-	str_dup = strdup(str);
-	if (str == NULL)
-	{
-		str_dup = strdup("(nil)");
-		if (str_dup == NULL)
-			return (NULL);
-		len = 0;
-	}
-	else if (str_dup == NULL)
-	{
+	str_dup = _strdup(str);
+	if (str_dup == NULL)
 		return (NULL);
-	}
 
-	/* Allocate memory for new node */
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
@@ -47,12 +76,9 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	/* Initialize new node */
 	new_node->str = str_dup;
-	new_node->len = len;
+	new_node->len = _strlen(str);
 	new_node->next = *head;
-
-	/* Update head to point to new node */
 	*head = new_node;
 
 	return (new_node);
