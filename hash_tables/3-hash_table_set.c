4c0c95ae84644dc2a82ee0b7fa50963c8f41c8f9
layan <layansaleem@gmail.com>
2025-10-21 07:26:37 +0000

    3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,6 +1,59 @@
 #include "hash_tables.h"
 
 /**
+ * create_new_node - Creates a new hash node
+ * @key: The key
+ * @value: The value
+ *
+ * Return: Pointer to new node, or NULL on failure
+ */
+hash_node_t *create_new_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+
+	return (new_node);
+}
+
+/**
+ * update_existing_node - Updates value of existing node
+ * @node: The node to update
+ * @value: The new value
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int update_existing_node(hash_node_t *node, const char *value)
+{
+	char *new_value;
+
+	new_value = strdup(value);
+	if (new_value == NULL)
+		return (0);
+
+	free(node->value);
+	node->value = new_value;
+	return (1);
+}
+
+/**
  * hash_table_set - Adds an element to the hash table
  * @ht: The hash table to add or update the key/value to
  * @key: The key (cannot be an empty string)
@@ -23,35 +76,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	while (temp != NULL)
 	{
 		if (strcmp(temp->key, key) == 0)
-		{
-			free(temp->value);
-			temp->value = strdup(value);
-			if (temp->value == NULL)
-				return (0);
-			return (1);
-		}
+			return (update_existing_node(temp, value));
 		temp = temp->next;
 	}
 
 	/* Create new node */
-	new_node = malloc(sizeof(hash_node_t));
+	new_node = create_new_node(key, value);
 	if (new_node == NULL)
 		return (0);
-
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
 
 	/* Add new node at the beginning of the list */
 	new_node->next = ht->array[index];
