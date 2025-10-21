7d1dee8215aeef548414931856a939aa8ba62ba3
layan <layansaleem@gmail.com>
2025-10-21 07:07:44 +0000

    0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,12 +11,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table = NULL;
 	unsigned long int i;
 
-	/* Allocate memory for the hash table structure */
+	if (size == 0)
+		return (NULL);
+
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
 
-	/* Allocate memory for the array of hash_node_t pointers */
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
 	{
@@ -24,11 +25,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 
-	/* Initialize each element of the array to NULL */
 	for (i = 0; i < size; i++)
 		new_table->array[i] = NULL;
 
-	/* Set the size of the hash table */
 	new_table->size = size;
 
 	return (new_table);
--- hash_tables/README.md
+++ hash_tables/README.md
@@ -5,16 +5,16 @@ This project implements a hash table data structure in C.
 ## Data Structures
 
 ### hash_node_t
-```c
-/**
- * struct hash_node_s - Node of a hash table
- * @key: The key (string)
- * @value: The value corresponding to the key
- * @next: A pointer to the next node of the list
- */
-typedef struct hash_node_s
-{
-    char *key;
-    char *value;
-    struct hash_node_s *next;
-} hash_node_t;
+Represents a node in the hash table with key, value, and next pointer.
+
+### hash_table_t
+Represents the hash table with size and array of pointers to nodes.
+
+## Functions
+
+### hash_table_create
+Creates a new hash table with the specified size.
+
+## Compilation
+```bash
+gcc -Wall -Werror -Wextra -pedantic *.c -o hash_tables
